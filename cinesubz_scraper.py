d0a1d088f42414a82046b97b17eda978f057fa06
Sayurami <sayuramihiranga4@gmail.com>
2025-05-22 11:44:05 +0530

    Update cinesubz_scraper.py

--- cinesubz_scraper.py
+++ cinesubz_scraper.py
@@ -1,29 +1,43 @@
+# cinesubz_scraper.py
+
 from flask import Flask, jsonify
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
-def scrape_cinesubz_movies():
+@app.route('/api/cinesubz-movies', methods=['GET'])
+def get_movies():
     url = 'https://cinesubz.co/movies/'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
+    headers = {
+        "User-Agent": "Mozilla/5.0"
+    }
 
-    movies = []
-    for movie_div in soup.find_all('div', class_='movie-thumb'):
-        title_tag = movie_div.find('h3')
-        link_tag = movie_div.find('a')
-        if title_tag and link_tag:
-            title = title_tag.text.strip()
-            link = link_tag['href']
-            movies.append({'title': title, 'link': link})
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # check for HTTP errors
+        soup = BeautifulSoup(response.text, 'html.parser')
 
-    return movies
+        movies = []
+        for movie_div in soup.find_all('div', class_='movie-thumb'):
+            title_tag = movie_div.find('h3')
+            link_tag = movie_div.find('a')
+            img_tag = movie_div.find('img')
 
-@app.route('/api/cinesubz-movies')
-def get_cinesubz_movies():
-    movies = scrape_cinesubz_movies()
-    return jsonify(movies)
+            if title_tag and link_tag:
+                title = title_tag.text.strip()
+                link = link_tag['href']
+                image = img_tag['src'] if img_tag else ''
+                movies.append({
+                    'title': title,
+                    'link': link,
+                    'image': image
+                })
+
+        return jsonify(movies)
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=3000)
