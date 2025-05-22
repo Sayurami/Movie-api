d6dece80ca0efd419ac66b8f54eb5b7e1aa3b7eb
Sayurami <sayuramihiranga4@gmail.com>
2025-05-22 21:42:22 +0530

    Create user.js

---
+++ user.js
@@ -0,0 +1,13 @@
+// users.js
+module.exports = [
+  {
+    username: 'gojo',
+    password: 'sixeyes123',
+    apiKey: 'abc123xyz789'
+  },
+  {
+    username: 'satoru',
+    password: 'infinity',
+    apiKey: 'def456lmn000'
+  }
+];
